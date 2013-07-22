0cc3f7811406091e3fb16d1385535b2797d9c787
boywonder123 <abhishek.mitra.a1@gmail.com>
2013-07-22 21:45:14 +0530

    Trying something

--- Webserver.py
+++ Webserver.py
@@ -1,28 +1,8 @@
-# -*- coding: cp1252 -*-
-import codecs
-from bottle import route, run, template, request
-import time
-import urllib2
-import urllib
-import json as m_json
 import os
-import sys
-from urllib import FancyURLopener
-import time
-from BeautifulSoup import BeautifulSoup
-import json as simplejson
-import sqlite3
-Soup = BeautifulSoup
-from nltk import sent_tokenize, word_tokenize
-from collections import Counter
-from math import log10
-# -*- coding: utf-8 *-*
+from flask import Flask
 
+app = Flask(__name__)
 
-
-profanity = ['fuck','asshole','sex','faggot','negro','nigger','boob','tit']
-@route('/')
-def login_form():
-    return "Hello World"
-
-
+@app.route('/')
+def hello():
+    return 'Hello World!'
