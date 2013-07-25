eadbeef5b70589b46a06af7264d9ce7454cd1ebb
boywonder123 <abhishek.mitra.a1@gmail.com>
2013-07-25 22:48:30 +0530

    Changes

--- Webserver.py
+++ Webserver.py
@@ -563,8 +563,8 @@ def ScienceSearch():
 
 @app.errorhandler(500)
 def pageNotFound(error):
-    nopage = ("<br><br><br><br><br><br>"+"<center><font size =6>Oops...your search timed out. Please refresh your page and try again.</font></center>"
-    return nopage
+    nopage = ("<br><br><br><br><br><br>"+"<center><font size =6>Oops...your search timed out. Please refresh your page and try again.</font></center>")
+    return (nopage)
 
 
 
