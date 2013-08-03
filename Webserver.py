db782abbbfd548cb0c907ca219d9e0fa0e7b5278
boywonder123 <abhishek.mitra.a1@gmail.com>
2013-08-03 21:06:06 +0530

    Changes to font

--- Webserver.py
+++ Webserver.py
@@ -47,7 +47,7 @@ def hello():
                      <br>
                      <br>
                      <br>
-                     Please send your feedback to: <a href="mailto:'admin@pacebyte.com'">Pacebyte administrator</a>
+                     Please send your feedback to: <a href="mailto:'admin@pacebyte.com'">admin@pacebyte.com</a>
                      <script>
                       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
