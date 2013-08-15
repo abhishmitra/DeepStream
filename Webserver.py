ab67f7f38af43b3f3a9933b72c4a429a2415a6a4
boywonder123 <abhishek.mitra.a1@gmail.com>
2013-08-15 12:36:41 +0530

    Bingimage

--- Webserver.py
+++ Webserver.py
@@ -25,12 +25,19 @@ profanity = ['fuck','asshole','sex','faggot','negro','nigger','boob','tit']
 
 URLA = "https://mykey:mykey@api.datamarket.azure.com/Bing/Search/Web?$format=json&Query=%(q)s"
 API_KEY = 'WdxgHLzMYWAsYipg/tv/RpK1mFk5YhuFeLQZxH2I1Uw'
-
+URLI = "https://mykey:mykey@api.datamarket.azure.com/Bing/Search/Image?$format=json&Query=%(q)s"
 def requester(que, **params):
     que = ('%27'+que+ '%27')
-    print que
     r = requests.get(URLA % {'q': que}, auth=('', API_KEY))
     return [res['Url'] for res in r.json()['d']['results']]
+
+def imagery(ima, **params):
+    print "Yolo"
+    print ima
+    r = requests.get(URLI % {'q': ima}, auth=('', API_KEY))
+    g = r.json()['d']['results']
+    print g[0]['Thumbnail']
+    return g[0]['Thumbnail']['MediaUrl']
 
 def splitParagraphIntoSentences(paragraph):
     import re
@@ -143,47 +150,14 @@ def PeopleSearch():
         acl = (len(adjCareer)-1)
         query = name
         raw = query
-    
         searchTerm = (raw)
-
-        searchTerm = searchTerm.replace(' ','%20')
         
-        print "hi"
-        class MyOpener(FancyURLopener): 
-            version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-        myopener = MyOpener()
-
-        i = 0
-        count= 0
-        
-        try:    
-         for i in range(0,1):
-            url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
-            reques1 = urllib2.Request(url, None, {'Referer': 'testing'})
-            response = urllib2.urlopen(reques1)
-            print "picture"
-            results = simplejson.load(response)
-            data = results['responseData']
-            print data
-            dataInfo = data['results']
-            print dataInfo
-            for myUrl in dataInfo:
-                count = count + 1
-                d = count
-                print myUrl['unescapedUrl']
-                picture = myUrl['unescapedUrl']
-                for i in range (0,2):
-                    if i is 2:
-                        a = myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-            myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-        except Exception,err:
-            pass
-
-
-    except Exception,err:
+        searchTerm = ('%27'+raw+'%27')
+        print "Hi"
+        picture = imagery(searchTerm)
+        print picture
+    except:
         pass
-
-#hi
 
     #Imports Text
     Education =""
@@ -353,88 +327,85 @@ def ScienceSearch():
     Mains = ""
     query = name
     raw = query
-    try:        
+            
     # Define search term
-        searchTerm = (raw)
+    searchTerm = (raw)
 
     # Replace spaces ' ' in search term for '%20' in order to comply with request
-        searchTerm = searchTerm.replace(' ','%20')
+    searchTerm = searchTerm.replace(' ','%20')
 
 
     # Start FancyURLopener with defined version 
-        class MyOpener(FancyURLopener): 
-            version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-        myopener = MyOpener()
+    class MyOpener(FancyURLopener): 
+        version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+    myopener = MyOpener()
 
-        i = 0
+    i = 0
     # Set count to 0
-        count= 0
+    count= 0
 
-        for i in range(0,1):
+    for i in range(0,1):
         # Notice that the start changes for each iteration in order to request a new set of images for each loop
-           url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
-           reques1 = urllib2.Request(url, None, {'Referer': 'testing'})
-           response = urllib2.urlopen(reques1)
+        url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
+        reques1 = urllib2.Request(url, None, {'Referer': 'testing'})
+        response = urllib2.urlopen(reques1)
 
         # Get results using JSON
-           results = simplejson.load(response)
-           data = results['responseData']
-           dataInfo = data['results']
+        results = simplejson.load(response)
+        data = results['responseData']
+        dataInfo = data['results']
 
         # Iterate for each result and get unescaped url
-           for myUrl in dataInfo:
-                count = count + 1
-                d = count
-                print myUrl['unescapedUrl']
-                picture = myUrl['unescapedUrl']
-                for i in range (0,2):
-                    if i is 2:
-                        a = myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-                myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-    except Exception,err:
-        pass
+        for myUrl in dataInfo:
+            count = count + 1
+            d = count
+            print myUrl['unescapedUrl']
+            picture = myUrl['unescapedUrl']
+            for i in range (0,2):
+                if i is 2:
+                    a = myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
+        myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
 
 #New Scan for formula
-    try:
+
      # Define search term
-        searchTerm = (raw + 'equation')
+    searchTerm = (raw + 'equation')
 
     # Replace spaces ' ' in search term for '%20' in order to comply with request
-        searchTerm = searchTerm.replace(' ','%20')
+    searchTerm = searchTerm.replace(' ','%20')
 
 
     # Start FancyURLopener with defined version 
-        class MyOpener(FancyURLopener): 
-            version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-        myopener = MyOpener()
+    class MyOpener(FancyURLopener): 
+        version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+    myopener = MyOpener()
 
-        i = 0
+    i = 0
     # Set count to 0
-        count= 0
+    count= 0
 
-        for i in range(0,1):
-            # Notice that the start changes for each iteration in order to request a new set of images for each loop
-            url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
-            reques1 = urllib2.Request(url, None, {'Referer': 'testing'})
-            response = urllib2.urlopen(reques1)
+    for i in range(0,1):
+        # Notice that the start changes for each iteration in order to request a new set of images for each loop
+        url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
+        reques1 = urllib2.Request(url, None, {'Referer': 'testing'})
+        response = urllib2.urlopen(reques1)
 
         # Get results using JSON
-            results = simplejson.load(response)
-            data = results['responseData']
-            dataInfo = data['results']
+        results = simplejson.load(response)
+        data = results['responseData']
+        dataInfo = data['results']
 
         # Iterate for each result and get unescaped url
-            for myUrl in dataInfo:
-                count = count + 1
-                d = count
-                print myUrl['unescapedUrl']
-                picfor = myUrl['unescapedUrl']
-                for i in range (0,2):
-                    if i is 2:
-                        a = myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-            myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-    except Exception,err:
-        pass
+        for myUrl in dataInfo:
+            count = count + 1
+            d = count
+            print myUrl['unescapedUrl']
+            picfor = myUrl['unescapedUrl']
+            for i in range (0,2):
+                if i is 2:
+                    a = myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
+        myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
+
 
     #scanning done
 
@@ -457,16 +428,20 @@ def ScienceSearch():
     v = 0
     Abst = "Nothing Here"
     jet = " "
-    quer= query
-    #First Search
-    o = requester(quer)
     
+    #First Search
+    quer =('"' +name+ '"')
+    print (quer)
+    o = requester(quer)
+    print o
     i = 0
     flag = 0
     Title = raw
-    Heading = ("<center><font size = 14> " + raw + "</font><br><br><img src='" + picture + "' width =400px><br><br></center>"+"<font size = 6 color = #0080FF><u>Concept:</font></u><br>" +"<img src='" + picfor + "' width =400px><br><br>")
+    Heading = ("<img src='https://lh5.googleusercontent.com/-cjODD9V7AXI/UerIKznIVNI/AAAAAAAAAaU/dSi288GNIr4/w500-h229-no/PaceByte.png' width = 200px> + <center><font size = 14> " + raw + "</font><br><br><img src='" + picture + "' width =400px></center><br><br>")
+    body = ""
+    Main = ""
     for f in range(0,len(o)-1):
-        if (f>15):
+        if (f>10):
             break
         print "Going"
         url = o[f]
@@ -530,8 +505,7 @@ def ScienceSearch():
                     pass
     
                                 
-        
-    
+     
 
     string = (Heading +"<font size = 6 color = #0080FF><u>"+"Summary:<br>"+"</u></font>" +"<font size=4>" +
               summary +"</font><br><br>" +"<font size = 6 color = #0080FF><u>Main Content:</u></font><br><br>"
@@ -543,6 +517,3 @@ def ScienceSearch():
 def pageNotFound(error):
     nopage = ("<br><br><br><br><br><br>"+"<center><font size =6>Oops...your search timed out. Please refresh your page and try again.</font></center>")
     return (nopage)
-
-app.run(host='localhost', port=8080)
-        
